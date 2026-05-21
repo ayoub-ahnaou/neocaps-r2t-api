39192feb1d4eb9276ee860ed399e030bb289ff25
ayoub-ahnaou <ayoubahnaou@gmail.com>
2026-05-21 12:06:35 +0100

    initial project structure with spring boot and sqlite as database

---
+++ src/main/java/com/r2p/neocaps/NeocapsR2pApplication.java
@@ -0,0 +1,13 @@
+package com.r2p.neocaps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeocapsR2pApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeocapsR2pApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/r2p/neocaps/NeocapsR2pApplicationTests.java
@@ -0,0 +1,13 @@
+package com.r2p.neocaps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeocapsR2pApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
