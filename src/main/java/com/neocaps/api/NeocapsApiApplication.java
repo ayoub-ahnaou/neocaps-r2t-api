66da3b2e4747c5968e782b4a983a3ccbf775ba33
ayoub-ahnaou <ayoubahnaou@gmail.com>
2026-05-31 20:14:32 +0100

    feat: initialize NeocapsApiApplication with Spring Boot setup

---
+++ src/main/java/com/neocaps/api/NeocapsApiApplication.java
@@ -0,0 +1,13 @@
+package com.neocaps.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeocapsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeocapsApiApplication.class, args);
+    }
+
+}
