8834c0cd8536e33ca545b0f019eef026514d823e
ayoub-ahnaou <ayoubahnaou@gmail.com>
2026-05-31 20:14:08 +0100

    feat: add custom exception handling with AppValidationException and ResourceNotFoundException, and implement GlobalExceptionHandler for centralized error responses

---
+++ src/main/java/com/neocaps/api/exception/AppValidationException.java
@@ -0,0 +1,11 @@
+package com.neocaps.api.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class AppValidationException extends RuntimeException {
+    public AppValidationException(String message) {
+        super(message);
+    }
+}
