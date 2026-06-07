9dfbfa9ee0580d8e5e016332b00aab43c15689f8
ayoub-ahnaou <ayoubahnaou@gmail.com>
2026-06-07 19:14:19 +0100

    feat: enable JPA auditing in NeocapsApiApplication for enhanced entity tracking

--- src/main/java/com/neocaps/api/NeocapsApiApplication.java
+++ src/main/java/com/neocaps/api/NeocapsApiApplication.java
@@ -2,8 +2,10 @@ package com.neocaps.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class NeocapsApiApplication {
 
     public static void main(String[] args) {
